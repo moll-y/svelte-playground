c15356ce392911094984829d58e76111e5797575
moll-y <mol.tired@gmail.com>
2023-02-07 04:49:18 -0500

    chore: install and configure tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav class="border-2 border-green-600">
+	<a href="/">Home</a>
+	<a href="/todos">Todos</a>
+</nav>
+
+<div class="border-2 border-blue-600">
+	<slot />
+</div>
